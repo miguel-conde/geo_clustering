0f63e93cb6dd71ad257c17fd17d68fd2ee489d1e
CONENTO\mconde <miguel.conde@conento.com>
2018-07-17 14:59:42 +0200

    Now plots with SSCC and ggplot

--- ggplot_ggmap.R
+++ ggplot_ggmap.R
@@ -1,8 +1,8 @@
 # http://mazamascience.com/WorkingWithData/?p=1494
 # http://www.kevjohnson.org/making-maps-in-r/
 
-library(ggmap)
 library(maptools)
+library(ggmap)
 library(tidyverse)
 
 # add to data a new column termed "id" composed of the rownames of data
@@ -85,3 +85,59 @@ SP + geom_polygon(data = SPFortified,
   scale_x_continuous(limits = c(b[1,1],b[1,2])) +
   scale_y_continuous(limits = c(b[2,1],b[2,2])) +
   theme(legend.position = "none", title = element_blank())
+
+
+#####
+cod_area <- "CE221" # ES123 NE522 NO411 SE312 SO312
+cod_area <- "ES223" # ES123 NE522 NO411 SE312 SO312 ES411
+cod_area <- "NE522" # ES123 NE522 NO411 SE312 SO312
+cod_area <- "NO411" # ES123 NE522 NO411 SE312 SO312
+cod_area <- "SE312" # ES123 NE522 NO411 SE312 SO312
+cod_area <- "SO312" # ES123 NE522 NO411 SE312 SO312
+
+
+sscc_sel_area <- SSCC_ONCE %>% 
+  filter(area_geografica == cod_area) %>% 
+  pull(seccode_2014)
+
+sscc_sel_area <- sscc_LL[sscc_LL$CUSEC %in% sscc_sel_area,]
+
+# BASE SYSTEM
+plot(sscc_sel_area, col = sscc_sel_area$CUSEC,
+     main = paste0("Secciones Censales del Área ",
+                   cod_area, "\n",
+                   final_dataset %>% 
+                        filter(area_geografica == cod_area) %>% 
+                        pull(descripcion_resumida)))
+
+# GGPLOT
+
+# add to data a new column termed "id" composed of the rownames of data
+sscc_sel_area@data$id <- rownames(sscc_sel_area@data)
+
+# reproject the data onto a "longlat" projection
+sscc_sel_area_trf <- spTransform(sscc_sel_area, CRS("+proj=longlat"))
+
+# create a data.frame from our spatial object
+sscc_sel_areaFortified <- fortify(sscc_sel_area_trf, region = "id")
+
+sscc_sel_areaFortified <- left_join(sscc_sel_areaFortified, 
+                                    sscc_sel_area_trf@data, by = "id")
+
+ggplot(data = sscc_sel_areaFortified,
+       aes(x = long, y = lat, 
+           fill = as.numeric(as.character(CUSEC)),
+           # fill = as.character(CUSEC), # PROBARLO !!!!
+           group = group
+           ), alpha = 0.5)  +
+  geom_polygon()  +
+  geom_path(color = "white") +
+  # scale_fill_gradient(low = "red", high = "blue") +
+  coord_equal() +
+  theme(axis.title = element_blank(), axis.text = element_blank()) +
+  labs(title = paste0("Secciones Censales del Área ",
+                      cod_area), 
+       subtitle = paste(final_dataset %>% 
+                          filter(area_geografica == cod_area) %>% 
+                          pull(descripcion_resumida)),
+       fill = "Area") 
